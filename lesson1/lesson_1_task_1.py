96fe132ed946c4319e7f5588d1e661c46f0fca77
IrinaSerebryak <ira_balakovo_@mail.ru>
2025-09-24 07:17:59 +0300

    lesson1

---
+++ lesson1/lesson_1_task_1.py
@@ -0,0 +1,40 @@
+my_heigh = 164
+print(my_heigh)
+
+my_name = "Ирина"
+print(my_name)
+
+
+
+
+def print_pithon():
+    print("Учу Pithon!")
+
+print_pithon()
+
+def print_letter(let):
+    print(let, end='')
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
