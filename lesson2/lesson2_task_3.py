8c1346011d0f9cd11389b6b64992947600c8025e
IrinaSerebryak <ira_balakovo_@mail.ru>
2025-10-01 22:45:31 +0300

    lesson2

---
+++ lesson2/lesson2_task_3.py
@@ -0,0 +1,15 @@
+def square(side):
+    """
+    Функция для вычисления площади квадрата.
+    Args:
+        side: Длина стороны квадрата (число)
+    Returns:
+        Площадь квадрата
+    """
+    return side * side
+if __name__ == "__main__":
+        print(square(8))
+        print(square(13))
+        print(square(15))
+
+
